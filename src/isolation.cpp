dd43d305b69b607edd83d289b419e939ed973499
Yousef Amar <yousefamar@gmail.com>
2014-02-13 00:07:28 +0000

    Detect edges with HOUGH TRANSFORM

--- src/isolation.cpp
+++ src/isolation.cpp
@@ -10,6 +10,24 @@
 
 using namespace std;
 
+cv::Mat hough_trans(cv::Mat input)
+{
+	cv::Mat edges, output;
+	cv::Canny(input, edges, 50, 200, 3);
+
+	cv::cvtColor(edges, output, cv::COLOR_GRAY2BGR);
+
+	vector<cv::Vec4i> lines;
+	cv::HoughLinesP(edges, lines, 1, CV_PI/180, 50, 50, 10 );
+	for(size_t i = 0; i < lines.size(); i++)
+	{
+		cv::Vec4i l = lines[i];
+		cv::line(output, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
+	}
+
+	return output;
+}
+
 /**
   * Find and perspective transform a card in an image
   *
@@ -21,5 +39,5 @@ using namespace std;
   */
 cv::Mat find_card(cv::Mat input)
 {
-	return input.clone();
+	return hough_trans(input);
 }
