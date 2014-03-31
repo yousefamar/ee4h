84e289eb99a07d14705df6fdff0c20d41691c574
Chris Lewis <bonsitm@gmail.com>
2014-03-31 19:22:54 +0100

    Card cascade fix and mixed image.

--- src/card.cpp
+++ src/card.cpp
@@ -198,10 +198,10 @@ void show_cascade(vector<Card> cards)
 	for (size_t i = 0; i < cards.size(); ++i)
 		cards[i].mat = cards[i].as_mat_with_results();
 
-	cv::Mat final((cards.size()/cards_per_row) * cards[0].mat.rows, min(cards.size() * cards[0].mat.cols, cards_per_row * cards[0].mat.cols), CV_8UC3);
+	cv::Mat final(((cards.size() - 1)/cards_per_row + 1) * cards[0].mat.rows, min(cards.size() * cards[0].mat.cols, cards_per_row * cards[0].mat.cols), CV_8UC3, cv::Scalar(0, 0, 0));
 
 	for (size_t i = 0; i < cards.size(); ++i)
-		cards[i].mat.copyTo(final(cv::Rect(cards[0].mat.cols * (i%cards_per_row), cards[0].mat.rows * (i/cards_per_row), cards[i].mat.cols, cards[i].mat.rows)));
+		cards[i].mat.copyTo(final(cv::Rect(cards[i].mat.cols * (i%cards_per_row), cards[i].mat.rows * (i/cards_per_row), cards[i].mat.cols, cards[i].mat.rows)));
 
 	//Finally
 	stringstream title_stream;
